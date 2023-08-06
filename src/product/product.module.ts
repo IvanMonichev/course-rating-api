56bed22aa916870b22b412dba4f7933633d55f78
IvanMonichev <risen15@yandex.ru>
2023-08-06 17:45:55 +0300

    feat: add connection to mongo DB

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ProductModel } from 'src/product/product.model';
 
 @Module({
-  controllers: [ProductController]
+  controllers: [ProductController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ProductModel,
+        schemaOptions: {
+          collection: 'Product'
+        }
+      },
+    ]),
+  ],
 })
 export class ProductModule {}
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ProductModel } from 'src/product/product.model';
+import { TopPageModel } from 'src/top-page/top-page.model';
 
 @Module({
-  controllers: [TopPageController]
+  controllers: [TopPageController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: TopPageModel,
+        schemaOptions: {
+          collection: 'TopPage'
+        }
+      },
+    ]),
+  ],
 })
 export class TopPageModule {}
