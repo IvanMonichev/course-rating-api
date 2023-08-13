cca5fdfd765a674ed83de406de27e6eccbe99bfc
IvanMonichev <risen15@yandex.ru>
2023-08-13 10:59:55 +0300

    feat: add actions

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = 8000;
+  const PORT = 4000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(PORT, () =>
